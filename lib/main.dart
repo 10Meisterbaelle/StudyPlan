ae4d9ce3a7116038f759f05a1402b8f9bba89398
10Meisterbaelle <10meisterbaelle@gmx.at>
2023-02-14 11:16:34 +0100

    Removed unneccesary template stuff

--- lib/main.dart
+++ lib/main.dart
@@ -49,18 +49,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _homeworkList = <String>["Test1", "Test2", "Test3"];
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
