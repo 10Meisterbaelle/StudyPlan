ff149c57e591af07d98bdeff863a3277c9e92f03
10Meisterbaelle <10meisterbaelle@gmx.at>
2023-02-15 11:24:50 +0100

    Reformatted main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'state.dart';
 
-final homeworkList = <String>["Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7", "Test8", "Test9", "Test10", "Test11", "Test12", "Test13", "Test14", "Test15", "Test16", "Test17", "Test18", "Test19", "Test20", "Test21", "Test22", "Test23", "Test24", "Test25"]; // Creates the list of homework
+final homeworkList = <String>[
+  "Test1",
+  "Test2",
+  "Test3",
+  "Test4",
+  "Test5",
+  "Test6",
+  "Test7",
+  "Test8",
+  "Test9",
+  "Test10",
+  "Test11",
+  "Test12",
+  "Test13",
+  "Test14",
+  "Test15",
+  "Test16",
+  "Test17",
+  "Test18",
+  "Test19",
+  "Test20",
+  "Test21",
+  "Test22",
+  "Test23",
+  "Test24",
+  "Test25"
+]; // Creates the list of homework
 // TODO: Don't forget to remove list items!
-final doneHomework = <int>{};
 
 void main() => runApp(const MyApp());
+final GlobalKey<MyHomePageState> myHomePageKey = GlobalKey<MyHomePageState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
