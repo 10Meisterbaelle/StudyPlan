f6fae40ea52dbe82f7fb897f55cdac0c04e2a986
10Meisterbaelle <10meisterbaelle@gmx.at>
2023-02-17 09:17:46 +0100

    Added extra database call

--- lib/state.dart
+++ lib/state.dart
@@ -38,6 +38,8 @@ class MyHomePageState extends State<MyHomePage> {
 
   void _saveItems() async {
     await database.saveStringList(homeworkList);
+    await database.close();
+    await database.open();
     setState(() {});
   }
 
